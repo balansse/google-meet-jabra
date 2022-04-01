8962cfcf496b2e5012923cd3fefd68d50b963f8d
Alexandr Balan <balansse@gmail.com>
2022-04-01 17:54:49 -0500

    Add dialog during call to request HID permissions; Fix for #1

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,88 +1,75 @@
-const JABRA_CONNECTING = "Connecting to Jabra device...";
+const JABRA_PARTNER_KEY = 'fafd-0b91d5be-f026-4198-b8b4-42685884d7ca';
+const JABRA_APP_ID = 'chrome-google-meet';
+const JABRA_APP_NAME = 'Google Meet Chrome extension';
 
-const MIC_OFF = {
-    de: "Mikrofon deaktivieren",
-    en: "Turn off microphone",
-    ja: "マイクをオフにする",
-};
-
-const MIC_ON = {
-    de: "Mikrofon aktivieren",
-    en: "Turn on microphone",
-    ja: "マイクをオンにする",
-};
-
-const END_CALL = {
-    en: "Leave call"
-}
+const JABRA_CONNECTING = 'Connecting to Jabra device...';
+const JABRA_HID_CONNECTION_REQUEST = 'Click here to connect to Jabra device';
+const JABRA_VENDOR_ID = 2830;
 
 const START_CALL = {
-    en: "Join now"
+    en: 'Join now'
 }
 
 const currentLanguage = () => document.documentElement.lang;
-const oldButtonSelector = (tip) => `[data-tooltip*='${tip}']`;
-const buttonSelector = (tip) => `[aria-label*='${tip}']`;
 
-const micOffButtonSelector = () =>
-    [
-        "[data-is-muted=false][aria-label*='+ d']",
-        "[data-is-muted=false][data-tooltip*='+ d']",
-        buttonSelector(MIC_OFF[currentLanguage()]),
-        oldButtonSelector(MIC_OFF[currentLanguage()]),
-    ].join(",");
-const micOffButton = () => document.querySelector(micOffButtonSelector());
+const getElementByIconName = (iconName, parentElement) => {
+    parentElement = parentElement !== undefined ? parentElement : document;
+    let element = Array.from(parentElement.querySelectorAll('i.google-material-icons')).find(el => el.textContent === iconName)
+    return element;
+}
 
-const micOnButtonSelector = () =>
-    [
-        "[data-is-muted=true][aria-label*='+ d']",
-        "[data-is-muted=true][data-tooltip*='+ d']",
-        buttonSelector(MIC_ON[currentLanguage()]),
-        oldButtonSelector(MIC_ON[currentLanguage()]),
-    ].join(",");
-const micOnButton = () => document.querySelector(micOnButtonSelector());
-
-const endCallSelector = () =>
-    [
-        buttonSelector(END_CALL[currentLanguage()]),
-        oldButtonSelector(END_CALL[currentLanguage()]),
-    ].join(",");
-const endCallButton = () => document.querySelector(endCallSelector());
-
+const micToggleButton = () => document.querySelector('button [data-icon-type="4"]')?.closest('button');
+const endCallButton = () => getElementByIconName('call_end')?.closest('button');
 const startCallButton = () => Array.from(document.querySelectorAll('span')).find(el => el.textContent === START_CALL[currentLanguage()]);
+    //Language-agnostic selectors for Start Call button
+    //Array.from(document.querySelectorAll('i.google-material-icons')).find(el => el.textContent === "present_to_all")?.closest('div[role="button"]').previousElementSibling
+    //document.querySelector('[data-is-prejoin]').parentElement.querySelector('[role="button"]')
 
-const avButtonSpan = () => Array.from(document.querySelectorAll('i.google-material-icons')).find(el => el.textContent === "inventory")?.closest('[role="row"]');
+const avButtonSpan = () => getElementByIconName('inventory')?.closest('[role="row"]');
 
 var jabraDevice;
 let jabraButton = { span: null, button: null, label: null };
 
 (async () => {
+
     const jabraApi = await jabra.init({
         transport: jabra.RequestedBrowserTransport.WEB_HID,
-        partnerKey: "fafd-0b91d5be-f026-4198-b8b4-42685884d7ca",
-        appId: "chrome-google-meet",
-        appName: "Google Meet Chrome extension",
+        partnerKey: JABRA_PARTNER_KEY,
+        appId: JABRA_APP_ID,
+        appName: JABRA_APP_NAME,
+        logger: {
+            write(logEvent) {
+              if (logEvent.level === 'error') {
+                console.log(logEvent.message, logEvent.layer);
+              }
+              // Ignore messages with other log levels
+            }
+          }
     });
 
-    if (avButtonSpan() !== undefined) {
-        jabraButton.span = avButtonSpan().cloneNode(true);
+    let span = avButtonSpan();
+
+    if (span !== undefined) {
+        jabraButton.span = span.cloneNode(true);
         jabraButton.button = jabraButton.span.querySelector('button');
         ['jscontroller', 'jsaction', 'jsname'].forEach(attribute => jabraButton.button.removeAttribute(attribute));
-        Array.from(jabraButton.span.querySelectorAll('i.google-material-icons')).find(el => el.textContent === "inventory").innerText = "headset_mic";
+
+        getElementByIconName('inventory', jabraButton.span).innerText = 'headset_mic';
+
         jabraButton.button.onclick = async (e) => {
-            await jabra.webHidPairing();
+            let connectedDevice = await jabra.webHidPairing();
+            if (connectedDevice && jabraDevice == undefined) {
+                jabraButton.label.innerText = JABRA_CONNECTING;
+            }
         };
 
+        let devices = await window.navigator.hid.getDevices();
+        let jabraHID = devices.find(element => element.vendorId === JABRA_VENDOR_ID);
+
         jabraButton.label = jabraButton.button.lastElementChild;
-        jabraButton.label.innerText = JABRA_CONNECTING;
+        jabraButton.label.innerText = jabraHID ? JABRA_CONNECTING : JABRA_HID_CONNECTION_REQUEST;
 
-        setTimeout(() => {
-            if (jabraButton.label.innerText === JABRA_CONNECTING) {
-                jabraButton.label.innerText = "Click here to connect";
-            }
-        }, 5000);
-
-        avButtonSpan().closest('div').append(jabraButton.span);
+        span.closest('div').append(jabraButton.span);
     }
 
     const callControlFactory = new jabra.CallControlFactory(jabraApi);
@@ -100,24 +87,28 @@ let jabraButton = { span: null, button: null, label: null };
 
         jabraDevice.deviceSignals.subscribe(
             (signal) => {
+                console.log(signal);
                 switch (signal.type) {
                     case jabra.SignalType.PHONE_MUTE:
-                        var button = micOffButton() || micOnButton();
+                        var button = micToggleButton();
                         button?.click();
                         break;
-
+                    
                     case jabra.SignalType.HOOK_SWITCH:
                         var button = signal.value ? startCallButton() : endCallButton();
                         button?.click();
+                        break;
+
+                    case jabra.SignalType.GN_PSEUDO_HOOK_SWITCH:
+                        startCallButton()?.click();
                         break;
                 }
             }
         );
 
-        for (const a of document.querySelectorAll('i.google-material-icons')) {
-            if (a.textContent.includes("call_end")) {
-                jabraDevice?.offHook(true);
-            }
+        //Instant meeting
+        if (endCallButton()) {
+            jabraDevice?.offHook(true);
         }
     });
 
@@ -126,7 +117,24 @@ let jabraButton = { span: null, button: null, label: null };
             if (change.target.classList.contains('google-material-icons')) {
                 for (const node of change.addedNodes) {
                     if (node.nodeType === Node.TEXT_NODE && node.data === 'call_end') {
+
+                        if (!jabraDevice) {
+                            fetch(chrome.runtime.getURL('/hid_dialog.html'))
+                            .then(r => r.text())
+                            .then(html => {
+                                document.querySelector('c-wiz').insertAdjacentHTML('beforeend', html);
+
+                                document.querySelector('.jabra-hid-dialog button').onclick = async (e) => {
+                                    let connectedDevice = await jabra.webHidPairing();
+                                    if (connectedDevice && jabraDevice == undefined) {
+                                        document.querySelector('.jabra-hid-dialog').remove();
+                                    }
+                                };
+                            });
+                        }
+
                         jabraDevice?.offHook(true);
+                            
                         return;
                     }
                 }
@@ -140,25 +148,27 @@ let jabraButton = { span: null, button: null, label: null };
 
     const callEndObserver = new MutationObserver((changes) => {
         for (const change of changes) {
-
-            for (const node of change.addedNodes) {
-                if (node.nodeType === Node.TEXT_NODE && node.data === 'You left the meeting') {
-                    jabraDevice?.offHook(false);
-                    return;
+            if (change.addedNodes.length > 0) {
+                for (const node of change.addedNodes) {
+                    if (node.nodeType === Node.ELEMENT_NODE) {
+                        let callEnded = node.querySelector('div[data-call-ended]')?.dataset.callEnded === 'true';
+                        if (callEnded === true) {
+                            jabraDevice?.offHook(false);
+                            return;
+                        }
+                    }
                 }
             }
-
         }
     });
     callEndObserver.observe(document.body, {
-        subtree: true,
-        childList: true,
+        childList: true, 
+        subtree: true
     });
 
     const muteObserver = new MutationObserver((changes) => {
         for (const change of changes) {
-
-            if (change.target === micOffButton() || micOnButton() && change.attributeName === "data-is-muted") {
+            if (change.target === micToggleButton() && change.attributeName === 'data-is-muted') {
                 jabraDevice?.mute(change.target.dataset.isMuted === 'true');
                 return;
             }
@@ -169,7 +179,7 @@ let jabraButton = { span: null, button: null, label: null };
         subtree: true,
     });
 
-    window.addEventListener("beforeunload", function () {
+    window.addEventListener('beforeunload', function () {
         jabraDevice?.offHook(false);
         jabraDevice?.releaseCallLock();
     });
